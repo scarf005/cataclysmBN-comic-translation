7541e88c0690139eef078a378da0ba4b35a69458
scarf <greenscarf005@gmail.com>
2022-07-04 00:11:46 +0900

    feat: rename files

---
+++ rename.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from pprint import pprint
+
+from flupy import flu
+
+res = (
+    flu(Path("연재").glob("*-*.xcf"))
+    .map(lambda e: (e, e.stem.split("-")))
+    .filter(lambda t: len(t[1]) == 2)
+    .filter(lambda t: t[1][1].isdigit())
+    .map(lambda t: (t[0], t[1][0], f"{t[1][1]:>02}"))
+    .sort(lambda t: (t[1], t[2]))
+    .map(lambda t: (t[0], f"연재/{t[1]}-{t[2]}.xcf"))
+    .filter(lambda t: str(t[0]) != t[1])
+    .collect()
+)
+
+pprint(res)
